49d3b123c0f0585bb2bdcae8ba8e6ca07e121f04
Natasha Cox <natashac1014@gmail.com>
2018-07-03 13:34:06 -0500

    edited and commented out

---
+++ src/scripts/article/articleModal.js
@@ -0,0 +1,9 @@
+// const createInputForm = require("../general/createInputForm")
+
+// let modalDiv = document.getElementById("addFormModal");
+// let modalParent = document.querySelector(".modal");
+
+// const displayArticleModal = () => {
+//     modalDiv.appendChild(createInputForm.articleForm());
+// };
+// module.exports = displayArticleModal
