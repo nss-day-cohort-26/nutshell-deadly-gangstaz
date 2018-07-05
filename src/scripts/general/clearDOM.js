dde69520dfff6d1fbb00a90ba921e917878e2ee9
Austin Gorman <austin.gorman@gmail.com>
2018-07-05 14:17:45 -0500

    fixed issue where everything reappended to DOM every time new info was submitted

--- src/scripts/general/clearDOM.js
+++ src/scripts/general/clearDOM.js
@@ -1,1 +1,9 @@
 // clears the dom
+const clear = (containerId) => {
+  let container = document.querySelector(containerId)
+  while (container.firstElementChild) {
+    container.removeChild(container.firstElementChild)
+  }
+}
+
+module.exports = clear;
